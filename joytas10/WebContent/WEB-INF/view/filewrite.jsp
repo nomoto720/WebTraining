23816296750bd4493a551c0eafa514efbcd75a92
nomoto <tka303@gmail.com>
2021-01-06 14:37:11 +0900

    1

---
+++ joytas10/WebContent/WEB-INF/view/filewrite.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<form action="/joytas10/FileWrite" method="post">
+memo:<input type="text" name="memo">
+<button type="submit">保存</button>
+</form>
+<a href="/joytas10/FileRead">一覧</a>
+</body>
+</html>
---
+++ joytas10/src/controller/FileWrite.java
@@ -0,0 +1,43 @@
+package controller;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/FileWrite")
+public class FileWrite extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd=request.getRequestDispatcher("/WEB-INF/view/filewrite.jsp");
+		rd.forward(request, response);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String memo=request.getParameter("memo");
+		ServletContext application=this.getServletContext();
+		String path=application.getRealPath("/WEB-INF/data/memo.txt");
+		System.out.println(path);
+		FileOutputStream fos=new FileOutputStream(path,true);
+		OutputStreamWriter osw=new OutputStreamWriter(fos,"sjis");
+		BufferedWriter bw=new BufferedWriter(osw);
+		Date now=new Date();
+		SimpleDateFormat sdf=new SimpleDateFormat("yyy-MM-dd HH:mm:ss");
+		String date=sdf.format(now);
+		bw.write(date);
+		bw.write("\t");
+		bw.write(memo);
+		bw.newLine();
+		bw.close();
+		response.sendRedirect("/joytas10/FileRead");
+	}
+
+}
